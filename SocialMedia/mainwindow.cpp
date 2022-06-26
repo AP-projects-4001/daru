a3fea885ed45a2e4b1c0adfecd9c9464f5b5236a
Nimazm <nima.1381.zamani@gmail.com>
2022-06-27 01:29:54 +0430

    Login procces

--- SocialMedia/mainwindow.cpp
+++ SocialMedia/mainwindow.cpp
@@ -2,6 +2,15 @@
 #include "ui_mainwindow.h"
 #include<iostream>
 #include"register.h"
+#include"user.h"
+#include"date.h"
+#include<iostream>
+#include<QJsonArray>
+#include<QJsonDocument>
+#include<QJsonObject>
+#include<QFile>
+#include<QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -19,5 +28,35 @@ void MainWindow::on_Register_Button_clicked()
 {
     Register* R=new Register(this);
     R->show();
+}
+
+
+void MainWindow::on_Login_Button_clicked()
+{
+    QJsonObject Test_User;
+    QJsonObject All_User;
+    QFile F_R_Users("All_User.json");
+    if(F_R_Users.open(QIODevice::ReadOnly))
+    {
+        QByteArray a = F_R_Users.readAll();
+        QJsonDocument b = QJsonDocument::fromJson(a);
+        All_User = b.object();
+        F_R_Users.close();
+        QList Users_Keys = All_User.keys();
+        for(int i = 0;i<Users_Keys.size();i++)
+        {
+            Test_User = All_User[Users_Keys.at(i).toLocal8Bit().constData()].toObject();
+            if(Test_User["Username"]==ui->User_Line->text()&&Test_User["Password"]==ui->Line_Pass->text())
+            {
+
+            }
+
+        }
+    }
+    else
+    {
+        QMessageBox::information(this,"Reason for Not working","cannot open the file");
+    }
+
 }
 
--- SocialMedia/register.cpp
+++ SocialMedia/register.cpp
@@ -8,6 +8,7 @@
 #include<QJsonObject>
 #include<QFile>
 #include<QMessageBox>
+#include<QDebug>
 Register::Register(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Register)
@@ -32,6 +33,7 @@ void Register::on_Register_Button_clicked()
         User *Reg_user = new User(ui->User_Line->text(),ui->Pass_Line->text(),ui->Phone_Line->text(),ui->Pass_Line->text(),ui->ID_Line->text(),ui->Birthday_Line->text());
         QJsonObject All_User;
         QFile F_R_Users("All_User.json");
+        int state=1;
         if(F_R_Users.open(QIODevice::ReadOnly))
         {
             QByteArray a = F_R_Users.readAll();
@@ -39,35 +41,49 @@ void Register::on_Register_Button_clicked()
             All_User = b.object();
             F_R_Users.close();
         }
+        QList Users_Keys = All_User.keys();
+        QJsonObject Current_User;
+        QJsonObject Test_User;
+        for(int i = 0;i<Users_Keys.size();i++)
+        {
+            Test_User = All_User[Users_Keys.at(i).toLocal8Bit().constData()].toObject();
+            if(Test_User["ID"]==Reg_user->getID()||Test_User["Phone"]==Reg_user->getPhoneNumber()||Test_User["Email"]==Reg_user->getEmail())
+            {
+                state=0;
+                break;
+            }
+
+        }
+        if(state==1)
+        {
+            Current_User["Username"] = Reg_user->getUserName();
+            Current_User["ID"] = Reg_user->getID();
+            Current_User["Birthday"] = Reg_user->getBirthDate();
+            Current_User["Phone"] = Reg_user->getPhoneNumber();
+            Current_User["Password"] = Reg_user->getPassword();
+            Current_User["Email"] = Reg_user->getEmail();
+            All_User[Reg_user->getID()]=Current_User;
+            QJsonDocument All_User_File(All_User);
+            QFile F_W_Users("All_User.json");
+            if(F_W_Users.open(QIODevice::WriteOnly))
+            {
+                F_W_Users.write(All_User_File.toJson());
+                F_W_Users.close();
+                this->destroy();
+                QMessageBox x;
+                x.setText("Registeration complete");
+                x.exec();
+            }
+            else{
+                QMessageBox x;
+                x.setText("Files Not Opening");
+                x.exec();
+            }
+        }
         else
         {
-            QMessageBox x;
-            x.setText("Files Not Opening");
-            x.exec();
-        }
-        QJsonObject Current_User;
-        Current_User["Username"] = Reg_user->getUserName();
-        Current_User["ID"] = Reg_user->getID();
-        Current_User["Birthday"] = Reg_user->getBirthDate();
-        Current_User["Phone"] = Reg_user->getPhoneNumber();
-        Current_User["Password"] = Reg_user->getPassword();
-        Current_User["Email"] = Reg_user->getEmail();
-        All_User[Reg_user->getID()]=Current_User;
-        QJsonDocument All_User_File(All_User);
-        QFile F_W_Users("All_User.json");
-        if(F_W_Users.open(QIODevice::WriteOnly))
-        {
-            F_W_Users.write(All_User_File.toJson());
-            F_W_Users.close();
+            QMessageBox::warning(this,"Reg_Problem","Same ID or Phone Number or Email");
             this->destroy();
-            QMessageBox x;
-            x.setText("Registeration complete");
-            x.exec();
-        }
-        else{
-            QMessageBox x;
-            x.setText("Files Not Opening");
-            x.exec();
         }
     }
     catch(out_of_range y)
@@ -76,7 +92,6 @@ void Register::on_Register_Button_clicked()
         x.setText(y.what());
         x.exec();
     }
-
 
 }
 
